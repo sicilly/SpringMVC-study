bc89f15663b16afd104a42d39cd8eac3239c486f
琬姗 曹 <634068240@qq.com>
2022-05-15 21:06:57 +0800

    拦截器

---
+++ springmvc-07-Interceptor/src/main/java/com/kuang/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.kuang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/t1")
+    public  String test(){
+        System.out.println("TestController-->Test()");
+        return "OK";
+    }
+}
